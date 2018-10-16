e0b693c11383942b31ad2e825ee9174e29ea6ed6
Victoria L Sumner <vicsumner@gmail.com>
2018-10-16 21:04:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist = {
-    artistName:"Alison",
-    songTitle: "Slowdrive"
+    // artistName:"Alison",
+    // songTitle: "Slowdrive"
   };
-      // playlist['Slowdive'] = 'Alison'
-      // playlist['My Bloody Valentine'] = 'Sometimes'
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
       
 function updatePlaylist(playlist, artistName, songTitle){
   return updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi")
