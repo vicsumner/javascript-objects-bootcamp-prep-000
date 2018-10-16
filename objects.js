73fc4b980ec8f9d6b667d73d08ac330e32dca2b8
Victoria L Sumner <vicsumner@gmail.com>
2018-10-16 20:52:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,8 @@ var playlist = {
     artistName:"Alison",
     songTitle: "Slowdrive"
   };
+  playlist.artistName =[]
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {artistName:songTitle})
+  return Object.assign({}, playlist, Phil Ochs, "Here's to the State of Mississippi")
   // updatePlaylist(playlist, "Sometimes", "My Bloody Valentine", "Phil Ochs", "Here's to the State of Mississippi")
  }
