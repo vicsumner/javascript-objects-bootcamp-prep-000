04e50943a114f5861a93b1e59b076be623ff1b2f
Victoria L Sumner <vicsumner@gmail.com>
2018-10-16 20:25:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {
     songTitle: "Slowdrive"
   };
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, artistName, songTitle)
+  return updatePlaylist(playlist, Phil Ochs, songTitle)
  }
